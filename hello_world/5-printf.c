b1a494f758a19004194a728588b8be45402b840c
Alexus Wilson <shalaywilson3695@gmail.com>
2023-05-29 11:34:45 -0700

    executable3

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
 *main - Print the text
 *
 *Return: Always 0.
+*
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art.\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

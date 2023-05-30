a7aaba36bf88afa16096f2eb9f7d924ace28497d
Alexus Wilson <shalaywilson3695@gmail.com>
2023-05-30 13:50:27 -0700

    print

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
  */
 int main(void)
-{
- int num;
 
- for (num = 0; num <= 9: num++)
 {
- putchar((num % 10) + '0');
- if (num == 9)
-   continue;
- putchar( ',');
- putchar( ' ' );
-}
-putchar('\n');
- 
-return (0);
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
 }

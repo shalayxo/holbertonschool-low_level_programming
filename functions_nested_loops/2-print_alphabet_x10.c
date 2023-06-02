e3223dede5ec859f3c12751f4a684a752df0bd69
Alexus Wilson <shalaywilson3695@gmail.com>
2023-06-02 11:30:39 -0700

    2-printalpha

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphapet -make the alphabet
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}

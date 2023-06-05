93f4c0c181a2fb51bc165f3950ffb1cc0ed43d9f
Alexus Wilson <shalaywilson3695@gmail.com>
2023-06-04 17:38:58 -0700

    isupper

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,26 +1,19 @@
-int _putchar(char c);
+int _isupper(int c);
 
-void  print_alphabet(void);
+int _isdigit(int c);
 
-void print_alphabet_x10(void);
+int mul(int a, int b);
 
-int _islower(int c);
+void print_numbers(void);
 
-int _isalpha(int c);
+void print_most_numbers(void);
 
-int print_sign(int n);
+void print_line(int n);
 
-int _abs(int);
+void print_diagonal(int n);
 
-int print_last_digit(int);
+void print_square(int size);
 
-void jack_bauer(void);
+void print_triangle(int size);
 
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
 ~                         

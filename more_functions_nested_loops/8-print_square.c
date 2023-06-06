748d53838c08670fb9793efa4b01792329459d82
Alexus Wilson <shalaywilson3695@gmail.com>
2023-06-05 21:58:52 -0700

    prdiagonal3

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: given variable
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int hort, vert;
+
+	if (size > 0)
+	(
+	 for (horz = 0; horz < size; horz++)
+		(
+		 	for (vert = 0; vert < size; vert++)
+			{
+				putchar('#');
+			}
+		putchar('\n');
+		}
+	}
+	else
+		putchar('\n');
+}

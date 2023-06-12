9c8396e2a7bd28ef4b5b93e5fa7bd882875e6e2f
shalayxo <shalaywilson3695@gmail.com>
2023-06-12 09:14:36 -0700

    lets

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - CHECK YOUR PROTOTYPE
+ * @x: BLAH
+ * Return: CHECK YOUR PROTOTYPE
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else if (x < 0)
+	{
+		return (-x);
+	}
+	else
+	{
+		return (9);
+	}
+}

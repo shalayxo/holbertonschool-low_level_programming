5f3c98b6e73ed2fd69438e725f7ef7f396e68a65
shalayxo <shalaywilson3695@gmail.com>
2023-06-13 20:52:42 -0700

    std

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,27 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_square - prints a square
- * @size: given variable
- *
- * Return: void
+ * print_square - Write a function that prints a square
+ * @size: is the size of the square
+ * Return: empty
  */
 void print_square(int size)
 {
-	int hort, vert;
+	int x, y;
 
-	if (size > 0)
-	(
-	 for (horz = 0; horz < size; horz++)
-		(
-		 	for (vert = 0; vert < size; vert++)
-			{
-				putchar('#');
-			}
+	if (size <= 0)
+	{
 		putchar('\n');
-		}
 	}
 	else
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				putchar(35);
+			}
 		putchar('\n');
+
+		}
+
 }

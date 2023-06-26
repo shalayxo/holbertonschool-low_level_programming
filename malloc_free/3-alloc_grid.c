93ec901cd8aa2c225c2a5edb7580c91bcae6dd69
shalayxo <shalaywilson3695@gmail.com>
2023-06-25 20:23:13 -0700

    4grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 		free(a);
 		return (NULL);
 	}
-
+	
 	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(sizeof(int) * width);
@@ -38,7 +38,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	
+
 	for (k = 0; k < height; k++)
 	{
 		for (l = 0; l < width; l++)

5a1abc7c8cb834738a553c02979271ce8a36f8d9
shalayxo <shalaywilson3695@gmail.com>
2023-07-24 20:33:20 -0700

    linked

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in the list
+ * @h: head of the list
+ * Return: return the Numberof elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+		h = h->next; 
+	return (i);
+}

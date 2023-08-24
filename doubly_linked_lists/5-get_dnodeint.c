8178fa09ec44d5c44e7deb8de1609d68086e6d1b
shalayxo <shalaywilson3695@gmail.com>
2023-08-23 21:56:59 -0700

    get

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,9 +9,9 @@
  * Otherwise -  the address of the located node.
  */
 
- dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	for (; index !=0; index--)
+	for (; index != 0; index--)
 	{
 		if (head == NULL)
 			return (NULL);
